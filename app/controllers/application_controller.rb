c29ac79afb887e1a32c042e1d4e692c78b29e68e
Natsuki-1004 <rstoyo.natsuki.yachita@gmail.com>
2023-09-20 11:26:48 +0900

    カラムを消したり増やしたり

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,20 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  
 
-    def configure_permitted_parameters
-      added_attrs = [ :nickname, :email, :password, :birthday ]
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    end
-  end
+    
 
 
   private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birhday])
+  end
+
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
   end
+end
 end
--- db/migrate/20230906091826_devise_create_users.rb
+++ db/migrate/20230906091826_devise_create_users.rb
@@ -3,11 +3,15 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :first_name_kana, null:false
+      t.string :last_name_kana, null:false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :nickname, null: false, default: ""
-      t.string :password, null: false, default: ""
       t.date :birhday, null: false
+      
       
       ## Recoverable
       t.string   :reset_password_token
