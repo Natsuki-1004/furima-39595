0f8bad8b6d88220f2d834917c7daf309700e1668
Natsuki-1004 <rstoyo.natsuki.yachita@gmail.com>
2023-09-23 21:58:44 +0900

    テーブルに保存できるように

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,19 @@ class ItemsController < ApplicationController
     def index
     end
 
+    def new
+      @item = Item.new
+    end
+
+    def create
+      @item = Item.new(item_params)
+        if @item.save
+          redirect_to root_path
+        else 
+          render :new, status: :unprocessable_entity
+        end
+        end
+        
   private
     def item_params
       params.require(:item).permit(:image).merge(user_id: current_user.id)
