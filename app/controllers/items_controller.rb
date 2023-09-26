01f4455c0d04674899b8c54ba57367d9418fdd75
Natsuki-1004 <rstoyo.natsuki.yachita@gmail.com>
2023-09-26 16:26:27 +0900

    edit修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   before_action :set_item, only: [:show, :edit, :update]
     
     def index
@@ -31,7 +31,6 @@ class ItemsController < ApplicationController
     end
 
     def edit
-      authenticate_user! 
       if current_user == @item.user
         render :edit
       else
